421fec82481ac80d04e230ac733f67fc6657a58f
Gatchan1 <r.barrio.f@gmail.com>
2023-10-17 16:12:51 +0200

    fixed footer border

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,6 @@
 export default function Footer() {
   return (
     <div id="Footer">
-      <hr />
       <div className="container">
         <p>Thanks for checking out my app!</p>
         <a href="https://github.com/Gatchan1/watchadoin-front"><img className="logo" src="/github-logo.png" alt="github logo"/>Github repo</a>
