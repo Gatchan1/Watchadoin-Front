ddc732a1431c31bf93bd7471399d0bdea4ea4600
Gatchan1 <r.barrio.f@gmail.com>
2024-08-28 12:57:12 +0200

    link to signup/login depending on route

--- src/components/NavbarLoggedOut.jsx
+++ src/components/NavbarLoggedOut.jsx
@@ -1,12 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function NavbarLoggedOut() {
+  let location = useLocation();
+
   return (
     <nav className="logged-out">
       <Link className="navbar-brand" to="/">
         <img src="/logo.png" />
       </Link>
-      <Link className="log-in" to="/login">Log in</Link>
+      {location.pathname == "/signup" && <Link className="log-in" to="/login">Log in</Link>}
+      {location.pathname == "/login" && <Link className="log-in" to="/signup">Sign up</Link>}
     </nav>
   );
 }
